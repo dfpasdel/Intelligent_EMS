1a23e11a0a8c5d7c9afe328fddffb6f98adc64aa
dfpasdel <d.pasdeloup@gmail.com>
2018-07-05 21:54:30 +0200

    Introduced negative reward for SOC < 25%

--- getReward.m
+++ getReward.m
@@ -4,19 +4,12 @@ function [r] = getReward(S)
 % NOTE: 
 % The Q-state is passed as a structure
 
-% switch true
-%     case S.SOC < 0.6
-%         r = 0;
-%     case S.SOC > 0.8
-%         r = 0;
-%     case 0.6 <= S.SOC <= 0.8
-%         r = 1;
-% end
 soc = S.SOC;
-if (0.68 <= soc) && (soc <= 0.72)
+if (0.65 <= soc) && (soc <= 0.75)
     r = 1;
+elseif (soc <= 0.25)
+    r = -1;
 else
     r = 0;
-    disp('fail');
 end 
 end
