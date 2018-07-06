0f379e5125e9391fd27c218934ce49a1c2379039
dfpasdel <d.pasdeloup@gmail.com>
2018-07-06 10:01:39 +0200

    Script generating a polynom to give reward as function of the SOC

---
+++ makeRewardCurveSOC.m
@@ -0,0 +1,16 @@
+% DESCRIPTION:
+% Fill values in x,y and check if the reward curve suits the reward
+% strategy desired.
+
+x = [0  0.15 0.25 0.6 0.65 0.7 0.75 0.8 0.9 0.98 1];
+y = [-2 -1.2 -1   0.5 0.9  1.8 0.9  0.5 0   -1  -2];
+
+rewardCurveSOC = polyfit(x,y,6);
+
+X = 0:0.02:1;
+Y = polyval(rewardCurveSOC,X);
+
+%figure(100)
+%plot(X,Y)
+
+save('rewardCurveSOC.mat',rewardCurveSOC);
