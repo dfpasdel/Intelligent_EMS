90f1c2f6892c3f775c72acfd4c52549bf5b75663
dfpasdel <d.pasdeloup@gmail.com>
2018-07-12 16:53:36 +0200

    Removed empiric correction

--- makeRewardCurveSOC.m
+++ makeRewardCurveSOC.m
@@ -5,14 +5,13 @@
 x = [0  0.15 0.25 0.6 0.65 0.7 0.75 0.8 0.9 0.98 1];
 y = [-2 -1.2 -1 0.5 0.9 1.8 0.9 0.5 0 -1 -2];
 
-% An empiric correction of -0.0257 is applied to center the curve around 0.7
-rewardCurveSOC = polyfit(x-0.0257,y,5);
+rewardCurveSOC = polyfit(x,y,5);
 
-X = 0:0.005:1;
+X = 0:0.001:1;
 Y = polyval(rewardCurveSOC,X);
 
-%figure(100)
-%plot(X,Y)
+% figure(100)
+% plot(X,Y)
 
 save('rewardCurveSOC.mat','rewardCurveSOC');
 
