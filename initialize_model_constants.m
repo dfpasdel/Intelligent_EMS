81f5d782d643fa4cb61176e83f6537a68c0ee32e
dfpasdel <d.pasdeloup@gmail.com>
2018-07-19 17:05:52 +0200

    Added more parameters for the battery

--- initialize_model_constants.m
+++ initialize_model_constants.m
@@ -6,7 +6,6 @@ function initialize_model_constants(model,MC)
 %
 % INPUTS: model, model constants (MC)
 
-
 assignin('base','MC',MC); % Simulink is working in the base WS
 
 load_system(model);
@@ -17,7 +16,7 @@ batteryMask = Simulink.Mask.get([model,'/Battery']);
 % In the Edit Mask window, the paramaters listed below have to be in
 % 'run-to-run' tunable mode to apply changes each time their value is
 % modified. See documentation.
-
+%%
 Nominal_voltage = batteryMask.Parameters(1,5); 
 Nominal_voltage.set('Value','MC.nominal_voltage');
 
@@ -29,5 +28,30 @@ Initial_SOC.set('Value','MC.initial_SOC');
 
 Battery_response_time = batteryMask.Parameters(1,8);
 Battery_response_time.set('Value','MC.battery_response_time');
+
+Checkbox = batteryMask.Parameters(1,9); 
+Checkbox.set('Value','MC.nominal_voltage');
+
+Max_capacity = batteryMask.Parameters(1,10); 
+Max_capacity.set('Value','MC.max_capacity');
+
+Cut_off_voltage = batteryMask.Parameters(1,11); 
+Cut_off_voltage.set('Value','MC.cut_off_voltage');
+
+Fully_charged_voltage = batteryMask.Parameters(1,12); 
+Fully_charged_voltage.set('Value','MC.fully_charged_voltage');
+
+Nominal_discharge_current = batteryMask.Parameters(1,13); 
+Nominal_discharge_current.set('Value','MC.nominal_discharge_current');
+
+Internal_R = batteryMask.Parameters(1,14); 
+Internal_R.set('Value','MC.internal_R');
+
+Capacity_at_nominal_voltage = batteryMask.Parameters(1,15); 
+Capacity_at_nominal_voltage.set('Value','MC.capacity_at_nominal_voltage');
+
+Exponential_zone = batteryMask.Parameters(1,16); 
+Exponential_zone.set('Value','MC.exponential_zone');
+
 
 end
