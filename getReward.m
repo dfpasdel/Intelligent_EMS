be361e569bccd85214c0d56ca591e3d9507e112a
dfpasdel <d.pasdeloup@gmail.com>
2018-07-25 12:43:05 +0200

    Managed interferences between concurrent rewards

--- getReward.m
+++ getReward.m
@@ -15,8 +15,13 @@ if (S.SOC <= 0.5) && (actionIdx ~= 3)... % Low SOC and ot incresasing the FC pow
         || (S.SOC >= 0.9) && (actionIdx ~= 2) % High SOC and not decreasing the FC power
     rSOC = rSOC - 0.2;
 end
+rSOC = 0.5 + 0.5*rSOC; % Rescale betwen [0;1] instead of [-1;1]
 
-rSOC = 0.5 + 0.5*rSOC; % Rescale betwen 0;1 instead of -1;1
+% Create a coefficient to be multiplicated with Battery and FC power
+% rewards to avoid interferences when SOC is bad.
+coef = 1.75 - abs(S.SOC-SOCtarget);
+coef = max(0,coef);
+coef = min(1,coef);
 
 % REWARD for the FC power
 if S.P_FC <= 0.8
@@ -24,10 +29,12 @@ if S.P_FC <= 0.8
 else
     rP_FC = 0;
 end
+rP_FC = rP_FC * coef;
 
 % REWARD for the Battery power
 rP_batt = 1.6 - 2*abs(S.P_batt);
 rP_batt = min(1,rP_batt);
 rP_batt = max(0,rP_batt);
+rP_batt = rP_batt * coef;
 
 end
