ccd669626dadf4b3b4c368f43af315bf7243918c
dfpasdel <d.pasdeloup@gmail.com>
2018-08-03 11:36:31 +0200

    Added more comments

--- getReward.m
+++ getReward.m
@@ -1,14 +1,27 @@
 function [rSOC,rP_FC,rP_batt,rSteady] = getReward(S,actionIdx)
 % DESCRIPTION:
 % Takes the Q-state as input and returns a reward as output.
+% This function is the Q-learning interpreter in ML words. It is of utmost 
+% importance for the problem to be solved and the settings have to be
+% chosen carefully
+%
 % NOTE: 
 % The Q-state is passed as a structure
 %
 % INPUTS:
-% - Structure containing the current Qstate
+% Structure containing the current Qstate
+% 
+% OUTPUTS:
+% The function outputs distinct rewards for each parameters. In order to
+% weight them in the calling function, the value of the reward outputed
+% here must be normalized between 0 and 1.
+% 
+% STATUS:
+% 03/08/18: Showed good results while setting a policy with SOC, P_Batt and
+% P_FC. The policy including the steady input has no good result yet.
 
 % REWARD for the SOC:
-SOCtarget = 0.7;
+SOCtarget = 0.7; % We aim the SOC centered on 70%
 rSOC = 0.5 - abs(S.SOC-SOCtarget)/0.3;
 if (S.SOC >= 0.65) && (S.SOC <= 0.75) %% && (actionIdx == 1)
     rSOC = rSOC * 2;
@@ -26,6 +39,7 @@ coef = max(0,coef);
 coef = min(1,coef);
 
 % REWARD for the FC power
+% We aim FC power lower than 0.8 p.u.
 if S.P_FC <= 0.8
     rP_FC = 1;
 else
